fc5fc854575038910bae184b78887adc4884fa92
hugebig16-alt <hugebig16@gmail.com>
2026-02-19 23:38:53 +0500

    Time: 0 ms (100.00%) | Memory: 12.9 MB (93.90%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,15 +1,16 @@
 class Solution:
     def searchInsert(self, nums, target):
-        left, right = 0, len(nums) - 1
+        left = 0  # начало массива
+        right = len(nums) - 1  # конец массива
 
-        while left <= right:
-            mid = (left + right) // 2
+        while left <= right:  # пока есть место для поиска
+            mid = (left + right) // 2  # берём середину
 
             if nums[mid] == target:
-                return mid
+                return mid  # нашли число, возвращаем его индекс
             elif nums[mid] < target:
-                left = mid + 1
+                left = mid + 1  # число больше, ищем справа
             else:
-                right = mid - 1
+                right = mid - 1  # число меньше, ищем слева
 
-        return left
+        return left  # число не найдено, возвращаем место, куда его можно вставить
