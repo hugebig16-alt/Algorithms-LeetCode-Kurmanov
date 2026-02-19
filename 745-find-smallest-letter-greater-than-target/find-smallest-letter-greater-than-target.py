e471f9a0e1ca1c35cd90901469519809caac382f
hugebig16-alt <hugebig16@gmail.com>
2026-02-20 00:14:53 +0500

    Time: 0 ms (100.00%) | Memory: 13.7 MB (69.57%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,15 +1,18 @@
 class Solution:
     def nextGreatestLetter(self, letters, target):
-        left, right = 0, len(letters) - 1
-        answer = None
+        left = 0  # начало массива
+        right = len(letters) - 1  # конец массива
+        answer = None  # здесь будем хранить подходящую букву
 
-        while left <= right:
-            mid = (left + right) // 2
+        while left <= right:  # пока есть место для поиска
+            mid = (left + right) // 2  # берём середину
 
             if letters[mid] > target:
-                answer = letters[mid]
-                right = mid - 1   # ищем меньший, но всё ещё > target
+                answer = letters[mid]  # нашли букву больше target, сохраняем её
+                right = mid - 1  # продолжаем искать ещё меньшую подходящую букву слева
             else:
-                left = mid + 1
+                left = mid + 1  # буква меньше или равна target, ищем справа
 
+        # если подходящая буква найдена, возвращаем её
+        # иначе возвращаем первую букву в массиве
         return answer if answer is not None else letters[0]
