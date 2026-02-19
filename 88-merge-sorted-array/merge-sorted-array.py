a58f081176c8cec39a14c1b3ba1c66be2d03a829
hugebig16-alt <hugebig16@gmail.com>
2026-02-19 21:42:21 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.89%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        i = m - 1          # указатель на конец реальных данных nums1
+        j = n - 1          # указатель на конец nums2
+        k = m + n - 1      # указатель на конец nums1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        # если остались элементы в nums2
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
